1df39240070866904ee9212b0957e525d806d222
Ant <ananthajit5d@gmail.com>
2023-07-31 00:01:57 +0530

    slope field now scales for nonlinear ODEs

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 These are a set of matlab scripts that I have made for small tasks
 
-- slope_field: Calculates the slope field given a function of the form $\frac{dy}{dx} = f(x)$, $\frac{dy}{dx} = f(y)$, $\frac{dy}{dx} = f(x, y)$
+- slope_field: Calculates the slope field given a function of the form $\frac{dy}{dx} = f(x)$, $\frac{dy}{dx} = f(y)$, $\frac{dy}{dx} = f(x, y). Tries to scale manually. I'd recommend using `dfield` since this struggles to scale vectors correctly.$
 
 - proj_gs_qr: Gram-Schmidt decomposition similar to one done by hand, not unlike QR decomposition.
--- slope_field/slope_field.m
+++ slope_field/slope_field.m
@@ -1,4 +1,4 @@
-function q = slope_field(func, t_range, y_range, t_intervals, y_intervals, f_type)
+function q = slope_field(func, t_range, y_range, t_intervals, y_intervals, f_type, varargin)
        
     assert( max(size(t_range))  == 2, "Only two values for t required, start and end")
     assert( max(size(y_range))  == 2, "Only two values for y required, start and end")
@@ -9,7 +9,7 @@ function q = slope_field(func, t_range, y_range, t_intervals, y_intervals, f_typ
     [T,Y] = meshgrid(t_vals, y_vals);
     
 
-    U = ones(size(T)) * t_intervals;
+    U = ones(size(T));
     if f_type == 1
         V = func(T);
     elseif f_type == 2
@@ -17,9 +17,36 @@ function q = slope_field(func, t_range, y_range, t_intervals, y_intervals, f_typ
     else % f_type == 3
         V = func(T,Y);
     end 
-    V = V.*U;
-    q = quiver(T,Y,U,V);
+%     V = V;
+
+    
+    
+
+    
+% manual scaling, looks kinda off though
+    
+
+    dist_mat = sqrt(U.^2 + V.^2);
+    max_dist = 0.95 * sqrt(t_intervals^2 + y_intervals^2);
+    min_dist = 0.5 * max_dist;
+    max_ind =  dist_mat > max_dist;
+    min_ind =  dist_mat < min_dist;
+    U(max_ind) = U(max_ind) ./ dist_mat(max_ind) * max_dist ;
+    V(max_ind) = V(max_ind) ./ dist_mat(max_ind) * max_dist ;
+    U(min_ind) = U(min_ind) ./ dist_mat(min_ind) * min_dist ;
+    V(min_ind) = V(min_ind) ./ dist_mat(min_ind) * min_dist ;
+    dist_mat = sqrt(U.^2 + V.^2);
+    wide = U > t_intervals;
+    U(wide) = U(wide) ./ dist_mat(wide) * t_intervals;
+    V(wide) = V(wide) ./ dist_mat(wide) * t_intervals;
+
+    tall = V > y_intervals;
+    U(tall) = U(tall) ./ dist_mat(tall) * y_intervals;
+    V(tall) = V(tall) ./ dist_mat(tall) * y_intervals;
+ 
+%     q = quiver(T,Y,U,V, varargin{:});
+    q = quiver(T,Y,U,V, "AutoScale", "off");
     q.ShowArrowHead = "off";
-    q; 
+    
     axis tight;
 end
